6b6ddbcdc8142d46f32fda7c139d4d37ce6926a4
zainab amjad <z_amjad@yahoo.com>
2017-01-08 09:03:43 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,31 +1,29 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  
+  my_hash={my_name:78 }
 end
 
 
 def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  #fill that hash with key/value pairs that describe the following information:
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+shipping_manifest={"whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3}
 
 end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
+shipping_manifest["oil paintings"]
   #your code here!
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  
+shipping_manifest["pearl necklace"]=1
+return shipping_manifest
   #your code here
   #remember to return the shipping_manifest hash
 
 end
-
-
-
